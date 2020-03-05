d6bc0c98548a859012f7a5c7e42b41c1e795cf65
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-05 18:19:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,8 +8,8 @@ class School
     @roster = {}
   end
   def add_student(student, grade)
-    roster[level] ||= []
-    roster[level] << student
+    roster[grade] ||= []
+    roster[grade] << student
   end
   
 end
